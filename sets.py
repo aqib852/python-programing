6279065078c6202e14d0d06bd7131701acd5b321
aqib852 <85600693+aqib852@users.noreply.github.com>
2025-12-05 15:00:18 +0530

    Update sets.py

--- sets.py
+++ sets.py
@@ -11,20 +11,20 @@ empty.add(12)
 
 # set methods
 empty.add(20)
-# print(empty)
-# print(len(empty))
-# thisset.remove(5)
-# print(thisset)
-# thisset.pop() # removes a random element
-# thiset.clear() #clears whole set
+print(empty)
+print(len(empty))
+thisset.remove(5)
+print(thisset)
+thisset.pop() # removes a random element
+thiset.clear() #clears whole set
 
-# print(thisset.union(empty))
-# s1= {1, 3, 4, 70}
-# s2 = {23, 40, 29}
-# print(s1.union(s2))
-# print(s1.intersection(s2))
-# s3= s1-s2
-# print(s3)
-# print({1.3}.issubset(s1))
+print(thisset.union(empty))
+s1= {1, 3, 4, 70}
+s2 = {23, 40, 29}
+print(s1.union(s2))
+print(s1.intersection(s2))
+s3= s1-s2
+print(s3)
+print({1.3}.issubset(s1))
 
 

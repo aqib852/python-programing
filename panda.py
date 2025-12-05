6c87f953b50a982f9544f0b94c3360fac206b413
aqib852 <85600693+aqib852@users.noreply.github.com>
2025-12-05 12:25:11 +0530

    Update panda.py

--- panda.py
+++ panda.py
@@ -86,6 +86,21 @@ import pandas as pd
 # print(df.info())
 
 #describe method most important
+import pandas as pd
+data = {
+    "name": [ "aqib", " jamdi ", "anayat"],
+    "age": [10,20,30],
+    "city":["baramulla", "anantnag","shupain"],
+    
+}
+df = pd.DataFrame(data)
+print(df)
+
+#save this as csv 
+df.to_csv("output.csv", index=False)
+
+
+
 data = {
     "name":["jam","aqib","anayat","mohit","mehwish","jafar","jd"],
     "age" : [20,24,23,43,32,33,31],

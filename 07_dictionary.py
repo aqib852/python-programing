1d286c0f5c09b2b78a7120d35ee09d979c434bd3
aqib852 <85600693+aqib852@users.noreply.github.com>
2025-12-05 14:48:19 +0530

    Update 07_dictionary.py

--- 07_dictionary.py
+++ 07_dictionary.py
@@ -1,66 +1,66 @@
-# dictionary is because of fast lookup
-# name = {
-#     "jamid":"fayaz",
-#       "aqib" : "yousuf",
-#       "anayat":"shupain",
-#       }
-# print(name)
-# thisdict = {
-#   "brand": "Ford",
-#   "model": "Mustang",
-#   "year": 1964
-# }
+dictionary is because of fast lookup
+name = {
+    "jamid":"fayaz",
+      "aqib" : "yousuf",
+      "anayat":"shupain",
+      }
+print(name)
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
 
-# print(type(thisdict))
-# print(thisdict.get("model"))
-# # thisdict.pop("year")
-# del thisdict["model"]
+print(type(thisdict))
+print(thisdict.get("model"))
+# thisdict.pop("year")
+del thisdict["model"]
 
-# print(thisdict.update({"model" : "ferrari"}))
-# copy = thisdict.copy()
-# print(copy)
+print(thisdict.update({"model" : "ferrari"}))
+copy = thisdict.copy()
+print(copy)
 
-# mydict = dict(copy)
-# print(mydict)
+mydict = dict(copy)
+print(mydict)
 
-# # nested dictionary
-# myfamily = {
-#     "child1" : {
-#         "name" : "aqib",
-#         "age" :  24
-#     },
-#     "child2" : {
-#         "name" : "sajid",
-#         "age" : 27
-#     }
-# }
-# print(myfamily)
-# print(myfamily.items())
+# nested dictionary
+myfamily = {
+    "child1" : {
+        "name" : "aqib",
+        "age" :  24
+    },
+    "child2" : {
+        "name" : "sajid",
+        "age" : 27
+    }
+}
+print(myfamily)
+print(myfamily.items())
 
-# # the differnece is if we execute whole code stops in simple
-# # print(myfamily.get("child2")) #print none
+# the differnece is if we execute whole code stops in simple
+# print(myfamily.get("child2")) #print none
 
-# # print(myfamily) # prints error
+# print(myfamily) # prints error
 
-# #it unordered 
-# # unindexed
-# # immutable
-# num_dict = {
-#     "0": "zero",
-#     "1": "one",
-#     "2": "two",
-#     "3": "three",
-#     "4": "four",
-#     "5": "five",
-#     "6": "six",
-#     "7": "seven",
-#     "8": "eight",
-#     "9": "nine"
-# }
+#it unordered 
+# unindexed
+# immutable
+num_dict = {
+    "0": "zero",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine"
+}
 
-# # Input from user
-# num = input("Enter a number: ")
-# output = " ".join(num_dict[d] for d in num)
+# Input from user
+num = input("Enter a number: ")
+output = " ".join(num_dict[d] for d in num)
 
 # print(output)
 message=input(">")

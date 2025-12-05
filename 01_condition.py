d3761b888a3b9ee0a383768e1f29fd4f7bf11059
aqib852 <85600693+aqib852@users.noreply.github.com>
2025-12-05 15:06:48 +0530

    Update 01_condition.py

--- 01_condition.py
+++ 01_condition.py
@@ -1,78 +1,78 @@
-# m = int(input("marks   in maths: "))
-# h= int(input("marks  in history: "))
-# s = int(input("marks  in science"))
+m = int(input("marks   in maths: "))
+h= int(input("marks  in history: "))
+s = int(input("marks  in science"))
 
-# total_percentage = (100)*(m+h+s)/300
-
-
+total_percentage = (100)*(m+h+s)/300
 
 
-# if(total_percentage>=40 and m>= 33 and h>=33 and s >=33):
-#     print("pass")
-# else:
-#     print("fail")
 
 
+if(total_percentage>=40 and m>= 33 and h>=33 and s >=33):
+    print("pass")
+else:
+    print("fail")
 
 
 
 
 
-# print("yes" if a>=18 else "no") advance coding
-
-# print(("no","yes")[a>=18])
-
-# if(a%2==0):
-#      print("no. is even")
-# else:
-#      print("jamid is lodu19")
-#     #  end of if statement no. 1
-
-# if (a>18):
-#      print("eligible for vote")
-# elif (a<=0):
-#      print("please enter a valid age")
-
-# else:
-#     print("not eligible")
-# # end of if statemnt no. 2
-
-# a =input("enter text here")
-# if a ==("make a lot of money"):
-#     print("this is a spam message")
-# elif a==("buy now"):
-#     print("this is a spam message")
-# elif a==("subscribe this"):
-#     print("this is a spam message")
-
-# username = input(" enter your name")
-
-# if (len(username)>10):
-#     print ("more than 10 charracters")
-# else:
-#     print( "less than 10 characters")
 
 
+print("yes" if a>=18 else "no") advance coding
+
+print(("no","yes")[a>=18])
+
+if(a%2==0):
+     print("no. is even")
+else:
+     print("jamid is lodu19")
+    #  end of if statement no. 1
+
+if (a>18):
+     print("eligible for vote")
+elif (a<=0):
+     print("please enter a valid age")
+
+else:
+    print("not eligible")
+# end of if statemnt no. 2
+
+a =input("enter text here")
+if a ==("make a lot of money"):
+    print("this is a spam message")
+elif a==("buy now"):
+    print("this is a spam message")
+elif a==("subscribe this"):
+    print("this is a spam message")
+
+username = input(" enter your name")
+
+if (len(username)>10):
+    print ("more than 10 charracters")
+else:
+    print( "less than 10 characters")
 
 
-# name = input("enter your name")
-# list=["aqib", "jamid" , "fayaz"]
-# if (name in list):
-#     print ("yes")
-# else:
-#     print ("no")
 
 
-# p1 ="hello"
-# p2 = "hi"
-# word =input("enter your word")
-# if ((p1 in word) or (p2 in word)) :
-#     print("spam")
-# else:
-#     print("not spam")
+name = input("enter your name")
+list=["aqib", "jamid" , "fayaz"]
+if (name in list):
+    print ("yes")
+else:
+    print ("no")
 
-# post= input("your post")
-# if ("harry".lower() in post.lower()): # .lower is used so that if we have haRry that will detect this also either uppern case or lower case
-#     print("yes")
-# else:
-#     print("no")
+
+p1 ="hello"
+p2 = "hi"
+word =input("enter your word")
+if ((p1 in word) or (p2 in word)) :
+    print("spam")
+else:
+    print("not spam")
+
+post= input("your post")
+if ("aqib".lower() in post.lower()): # .lower is used so that if we have haRry that will detect this also either uppern case or lower case
+    print("yes")
+else:
+    print("no")

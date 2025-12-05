bd92a327650dfb1130bda647cfd428f6bc4261e3
aqib852 <85600693+aqib852@users.noreply.github.com>
2025-12-05 14:59:26 +0530

    Update recurssion.py

--- recurssion.py
+++ recurssion.py
@@ -1,30 +1,30 @@
-# def factorial(n):
-#     if n==1 or n==1:
-#         return 1
-#     return n*factorial(n-1) #function calls itself again and again
-# 
-# print(f"the facorial of the nomber {n} is : {factorial(n)}")
+def factorial(n):
+    if n==1 or n==1:
+        return 1
+    return n*factorial(n-1) #function calls itself again and again
 
-# def gratest():
-#     if a>b and a>c:
-#         return a
-#     elif b>a and b>c:
-#         return b
-#     else:
-#         return c
-# a = int (input("enter the the number 2: "))
-# b = int (input("enter the the number 2: "))
-# c= int (input("enter the the number 3: "))
-# print(f"the greater number is  : {gratest()}")
+print(f"the facorial of the nomber {n} is : {factorial(n)}")
 
-# def sum(n):
-#     if n == 0:
-#         return 0
-#     # else:
-#     #     return 0
-#     return  n + sum(n-1)
-# n = int (input("enter the the number 2: "))
-# print(f"the sum is : {sum(n)}")
+def gratest():
+    if a>b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    else:
+        return c
+a = int (input("enter the the number 2: "))
+b = int (input("enter the the number 2: "))
+c= int (input("enter the the number 3: "))
+print(f"the greater number is  : {gratest()}")
+
+def sum(n):
+    if n == 0:
+        return 0
+    # else:
+    #     return 0
+    return  n + sum(n-1)
+n = int (input("enter the the number 2: "))
+print(f"the sum is : {sum(n)}")
     
 def fib(n):
     if n<=1:

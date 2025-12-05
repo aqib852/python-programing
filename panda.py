4abef20dadaa3f6e89e341094ab36716a2e65575
aqib852 <85600693+aqib852@users.noreply.github.com>
2025-12-05 14:51:23 +0530

    Update panda.py

--- panda.py
+++ panda.py
@@ -21,57 +21,57 @@ import pandas as pd
 
 
 #series
-# temp = pd.Series([12,23,45,67,78,34,45],
-#                  index = ["mon", "tue", "wed", "thur", "fri", "sat","sun"])
-# print(temp["mon"])
-# print(temp)
-# print(temp.max())
-# print(temp.min())
-# print(temp.iloc[0:3]) #integer index locator
-# print(temp.loc["mon"])
-# print(temp.mean())
-# print(temp.sort_index())
-# print(temp.sort_values())
-# print(temp[temp>12]) #we can directly add condition here
-# print(dir(max))
+temp = pd.Series([12,23,45,67,78,34,45],
+                 index = ["mon", "tue", "wed", "thur", "fri", "sat","sun"])
+print(temp["mon"])
+print(temp)
+print(temp.max())
+print(temp.min())
+print(temp.iloc[0:3]) #integer index locator
+print(temp.loc["mon"])
+print(temp.mean())
+print(temp.sort_index())
+print(temp.sort_values())
+print(temp[temp>12]) #we can directly add condition here
+print(dir(max))
 
 #read data from  CSV file into data frame
-# df = pd.read_csv("sales_data_sample.csv",encoding= "latin1")  #encoding="utf-8" or = "latin1") ---> encoding is used only  when we get an error while reading the file
-# # for large data set we can use in chunks using for loop
+df = pd.read_csv("sales_data_sample.csv",encoding= "latin1")  #encoding="utf-8" or = "latin1") ---> encoding is used only  when we get an error while reading the file
+# for large data set we can use in chunks using for loop
 
 
-# df2 = pd.read_excel("samplesuperstore.xlsx")
-# df3 = pd.read_json("sample_Data.json")                 
-# print(df)
-# print(df2)
-# print(df3)
+df2 = pd.read_excel("samplesuperstore.xlsx")
+df3 = pd.read_json("sample_Data.json")                 
+print(df)
+print(df2)
+print(df3)
 
 
 #create own data
 
-# data = {
-#     "name": [ "aqib", " jamdi ", "anayat"],
-#     "age": [10,20,30],
-#     "city":["baramulla", "anantnag","shupain"],
+data = {
+    "name": [ "aqib", " jamdi ", "anayat"],
+    "age": [10,20,30],
+    "city":["baramulla", "anantnag","shupain"],
     
-# }
-# df = pd.DataFrame(data)
-# print(df)
+}
+df = pd.DataFrame(data)
+print(df)
 
-# #save this as csv 
-# df.to_csv("output.csv", index=False)
+#save this as csv 
+df.to_csv("output.csv", index=False)
 
 
 
 
 #understand rows
 # 2 methods head(n) :first n rows  , tail(n):display last n rows print("display 10 rows")print("display 10 rows")
-# print("display first 10 rows")
-# print(df.head(10)) # by default ist 5 rows 
+print("display first 10 rows")
+print(df.head(10)) # by default ist 5 rows 
 
-# print("last 10 rows") # by default last 5 rows
+print("last 10 rows") # by default last 5 rows
 
-# print(df.tail(10))
+print(df.tail(10))
 
 
 
